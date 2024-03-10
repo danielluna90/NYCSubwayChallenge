2b784c48c5d912a5fc40fb43431ef990ed0f5393
Daniel Luna <l2318dan@gmail.com>
2024-03-10 13:56:10 -0400

    Remove extra status

--- server/src/commands/parser.cpp
+++ server/src/commands/parser.cpp
@@ -37,8 +37,6 @@ CommandType CommandParser::getCommandType(std::string const& str) {
         return CommandType::COMMAND_HELP;
     } else if (str == "status") {
         return CommandType::COMMAND_STATUS;
-    } else if (str == "status") {
-        return CommandType::COMMAND_STATUS;
     } else if (str == "enable") {
         return CommandType::COMMAND_ENABLE;
     } else if (str == "disable") {
